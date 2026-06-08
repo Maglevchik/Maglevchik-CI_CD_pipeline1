00ea91dd6fd3526e2d7e1c982ef3e1fb952fac91
Maglevchik <mihael570521@gmail.com>
2026-06-08 14:32:28 +0300

    Update settings.py

--- bot_builder/settings.py
+++ bot_builder/settings.py
@@ -76,16 +76,27 @@ WSGI_APPLICATION = 'bot_builder.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': 'db',   # 'db' — это имя сервиса из нашего docker-compose.yml
-        'PORT': '5432', # Стандартный порт PostgreSQL
+if os.environ.get('CI') == 'true':
+    # Если код запущен в GitHub, используем легкую файловую БД
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    # Иначе используем твой боевой PostgreSQL
+    # ВАЖНО: Вставь ниже СВОИ текущие настройки, которые у тебя уже были написаны!
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv('DB_NAME'),
+            'USER': os.getenv('DB_USER'),
+            'PASSWORD': os.getenv('DB_PASSWORD'), 
+            'HOST': 'db',
+            'PORT': 5432,
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
